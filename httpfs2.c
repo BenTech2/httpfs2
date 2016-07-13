a9d073e157cb6f84829ace46cbaf52f723b16f13
Michal Suchánek <787652+hramrach@users.noreply.github.com>
2016-07-13 09:35:01 +0200

    make ssl prints use stderr to match rest of prints.

--- httpfs2.c
+++ httpfs2.c
@@ -452,14 +452,14 @@ print_ssl_info (gnutls_session_t session)
     int dhe, ecdh;
     dhe = ecdh = 0;
     if (!session) {
-        printf("No SSL session data.\n");
+        fprintf(stderr, "No SSL session data.\n");
         return 0;
     }
     /* print the key exchange’s algorithm name
     */
     kx = gnutls_kx_get (session);
     tmp = gnutls_kx_get_name (kx);
-    printf ("- Key Exchange: %s\n", tmp);
+    fprintf(stderr, "- Key Exchange: %s\n", tmp);
     /* Check the authentication type used and switch
      * to the appropriate.
      */
@@ -479,44 +479,44 @@ print_ssl_info (gnutls_session_t session)
             /* cert should have been printed when it was verified */
             break;
         default:
-            printf("Not a x509 sesssion !?!\n");
+            fprintf(stderr, "Not a x509 sesssion !?!\n");
 
     }
 #if (GNUTLS_VERSION_MAJOR > 3 )
     /* switch */
     if (ecdh != 0)
-        printf ("- Ephemeral ECDH using curve %s\n",
+        fprintf(stderr, "- Ephemeral ECDH using curve %s\n",
                 gnutls_ecc_curve_get_name (gnutls_ecc_curve_get (session)));
     else
 #endif
         if (dhe != 0)
-            printf ("- Ephemeral DH using prime of %d bits\n",
+            fprintf(stderr, "- Ephemeral DH using prime of %d bits\n",
                     gnutls_dh_get_prime_bits (session));
     /* print the protocol’s name (ie TLS 1.0)
     */
     tmp = gnutls_protocol_get_name (gnutls_protocol_get_version (session));
-    printf ("- Protocol: %s\n", tmp);
+    fprintf(stderr, "- Protocol: %s\n", tmp);
     /* print the certificate type of the peer.
      * ie X.509
      */
     tmp =
         gnutls_certificate_type_get_name (gnutls_certificate_type_get (session));
-    printf ("- Certificate Type: %s\n", tmp);
+    fprintf(stderr, "- Certificate Type: %s\n", tmp);
     /* print the compression algorithm (if any)
     */
     tmp = gnutls_compression_get_name (gnutls_compression_get (session));
-    printf ("- Compression: %s\n", tmp);
+    fprintf(stderr, "- Compression: %s\n", tmp);
     /* print the name of the cipher used.
      * ie 3DES.
      */
     tmp = gnutls_cipher_get_name (gnutls_cipher_get (session));
-    printf ("- Cipher: %s\n", tmp);
+    fprintf(stderr, "- Cipher: %s\n", tmp);
     /* Print the MAC algorithms name.
      * ie SHA1
      */
     tmp = gnutls_mac_get_name (gnutls_mac_get (session));
-    printf ("- MAC: %s\n", tmp);
-    printf ("Note: SSL paramaters may change as new connections are established to the server.\n");
+    fprintf(stderr, "- MAC: %s\n", tmp);
+    fprintf(stderr, "Note: SSL paramaters may change as new connections are established to the server.\n");
     return 0;
 }
 
@@ -549,17 +549,17 @@ verify_certificate_callback (gnutls_session_t session)
         return GNUTLS_E_CERTIFICATE_ERROR;
     }
     if (status & GNUTLS_CERT_INVALID)
-        printf ("The server certificate is NOT trusted.\n");
+        fprintf(stderr, "The server certificate is NOT trusted.\n");
     if (status & GNUTLS_CERT_INSECURE_ALGORITHM)
-        printf ("The server certificate uses an insecure algorithm.\n");
+        fprintf(stderr, "The server certificate uses an insecure algorithm.\n");
     if (status & GNUTLS_CERT_SIGNER_NOT_FOUND)
-        printf ("The server certificate hasn’t got a known issuer.\n");
+        fprintf(stderr, "The server certificate hasn’t got a known issuer.\n");
     if (status & GNUTLS_CERT_REVOKED)
-        printf ("The server certificate has been revoked.\n");
+        fprintf(stderr, "The server certificate has been revoked.\n");
     if (status & GNUTLS_CERT_EXPIRED)
-        printf ("The server certificate has expired\n");
+        fprintf(stderr, "The server certificate has expired\n");
     if (status & GNUTLS_CERT_NOT_ACTIVATED)
-        printf ("The server certificate is not yet activated\n");
+        fprintf(stderr, "The server certificate is not yet activated\n");
     /* Up to here the process is the same for X.509 certificates and
      * OpenPGP keys. From now on X.509 certificates are assumed. This can
      * be easily extended to work with openpgp keys as well.
@@ -574,7 +574,7 @@ verify_certificate_callback (gnutls_session_t session)
     cert_list = gnutls_certificate_get_peers (session, &cert_list_size);
     if (cert_list == NULL)
     {
-        printf ("No server certificate was found!\n");
+        fprintf(stderr, "No server certificate was found!\n");
         return GNUTLS_E_CERTIFICATE_ERROR;
     }
     /* Check the hostname matches the certificate.
@@ -586,7 +586,7 @@ verify_certificate_callback (gnutls_session_t session)
         return GNUTLS_E_CERTIFICATE_ERROR;
     }
     if (!(url->ssl_connected)) if (!gnutls_x509_crt_print (cert, GNUTLS_CRT_PRINT_FULL, &data)) {
-        printf("%s", data.data);
+        fprintf(stderr, "%s", data.data);
         gnutls_free(data.data);
     }
     if (!hostname || !gnutls_x509_crt_check_hostname (cert, hostname))
@@ -596,7 +596,7 @@ verify_certificate_callback (gnutls_session_t session)
             int i;
             size_t len = strlen(hostname);
             if (*(hostname+len-1) == '.') len--;
-            if (!(url->ssl_connected)) printf ("Server hostname verification failed. Trying to peek into the cert.\n");
+            if (!(url->ssl_connected)) fprintf(stderr, "Server hostname verification failed. Trying to peek into the cert.\n");
             for (i=0;;i++) {
                 char * dn = NULL;
                 size_t dn_size = 0;
@@ -612,7 +612,7 @@ verify_certificate_callback (gnutls_session_t session)
                         if (len == dn_size)
                             match = ! strncmp(dn, hostname, len);
                         if (match) found = 1;
-                        if (!(url->ssl_connected)) printf("Cert CN(%i): %s: %c\n", i, dn, match?'*':'X');
+                        if (!(url->ssl_connected)) fprintf(stderr, "Cert CN(%i): %s: %c\n", i, dn, match?'*':'X');
                     }}
                 else
                     ssl_error(dn_ret, session, "getting cert subject data");
@@ -622,7 +622,7 @@ verify_certificate_callback (gnutls_session_t session)
             }
         }
         if(!found){
-            printf ("The server certificate’s owner does not match hostname ’%s’\n",
+            fprintf(stderr, "The server certificate’s owner does not match hostname ’%s’\n",
                     hostname);
             return GNUTLS_E_CERTIFICATE_ERROR;
         }
